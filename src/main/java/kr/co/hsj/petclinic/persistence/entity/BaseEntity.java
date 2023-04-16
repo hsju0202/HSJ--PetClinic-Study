794eb49f39f08c2152145dc37e4306f615f32017
sochong <hsju0202@gmail.com>
2023-04-16 20:54:52 +0900

    Fix: fix BaseEntity
    - changing member variable's access level
    - add Getter

--- src/main/java/kr/co/hsj/petclinic/persistence/entity/BaseEntity.java
+++ src/main/java/kr/co/hsj/petclinic/persistence/entity/BaseEntity.java
@@ -4,12 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    protected Long id;
 
 }
